380615681ae3a4fbe16e4ebb727fa7e842cca747
Kevin <kienluproductions@gmail.com>
2024-05-24 11:16:15 +0700

    first commit

---
+++ CalendarExample/CalendarExampleApp.swift
@@ -0,0 +1,19 @@
+//
+//  CalendarExampleApp.swift
+//  CalendarExample
+//
+//  Created by Kevin on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CalendarExampleApp: App {
+//    @State var selectedDate: Date? = Date()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarExample/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  CalendarExample
+//
+//  Created by Kevin on 5/23/24.
+//
+
+import SwiftUI
+import CalendarUI
+
+struct ContentView: View {
+    @State var selectedDate: Date? = Date()
+
+    var body: some View {
+        CalendarView(selectedDate: $selectedDate)
+    }
+}

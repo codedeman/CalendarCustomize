22277007d031320270a50fd8cd12a598f1de6131
Kevin <kienluproductions@gmail.com>
2024-06-05 14:02:23 +0700

    update xcframework

--- SPM/Package.swift
+++ SPM/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CalendarControl",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
